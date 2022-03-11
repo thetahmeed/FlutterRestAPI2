19f5299a7fbc7e1216199a5f07c1062b33944855
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-03-11 18:37:13 +0600

    Update api_services.dart

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,5 +1,8 @@
 import 'package:api_with_flutter/models/post.dart';
 
+// 2. Imported the http package
+import 'package:http/http.dart' as http;
+
 class ApiServices {
   Future<List<Post>?> getPosts() async {}
   // 1. Created the fuction to get all posts
