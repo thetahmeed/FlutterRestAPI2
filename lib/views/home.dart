78216c09cb354aceaed8e25356a07aa1d17eb9a8
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-03-11 20:02:07 +0600

    Assigning value to the list

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -11,6 +11,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Post>? posts;
+    posts = await ApiServices().getPosts();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
