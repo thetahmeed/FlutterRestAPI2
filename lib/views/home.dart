15ec74ebbe4e6f480715f1163d0f14ec6fe6ccef
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-03-11 20:01:26 +0600

    Created list type of post

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,3 +1,5 @@
+import 'package:api_with_flutter/models/post.dart';
+import 'package:api_with_flutter/services/api_services.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,18 +10,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Post>? posts;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home Page"),
       ),
-      body: ListView.builder(
-        itemCount: 12,
-        itemBuilder: (context, index) {
-          return Text('ListView item $index');
-        },
-      ),
+      body: isLoaded
+          ? ListView.builder(
+              itemCount: posts?.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(posts![index].id.toString()),
+                      Text('Title: ' + posts![index].title.toString()),
+                      Text('Body: ' + posts![index].body.toString()),
+                      const SizedBox(height: 8),
+                    ],
+                  ),
+                );
+              },
+            )
+          : const Center(child: CircularProgressIndicator()),
     );
   }
 }
